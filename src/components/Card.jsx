d25cfb10d0df3de8a98e223692838c7a61bd7b65
Lakshya Singh <lakshyasinght2003@gmail.com>
2023-10-05 07:27:08 +0530

    new

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/card.css';
-import { Navigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Card = (props) => {
   return (
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,5 +1,4 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
-import App from '../App';
 import PropertyDetails from '../pages/PropertyDetails';
 
 const Routers = () => {
